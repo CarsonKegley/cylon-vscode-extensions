3d90e5cd4a2fb24894c0f23c7856f79d5a349b74
Carson Kegley <carson.kegley@gmail.com>
2021-04-06 12:28:25 -0400

    Pruned existing extensions list

---
+++ vscodeExtensionsInstall.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+code --install-extension dbaeumer.vscode-eslint
+code --install-extension eamodio.gitlens
+code --install-extension ecmel.vscode-html-css
+code --install-extension esbenp.prettier-vscode
+code --install-extension GabrielBB.vscode-lombok
+code --install-extension ms-azuretools.vscode-docker
+code --install-extension ms-vscode-remote.remote-wsl
+code --install-extension ms-vscode.powershell
+code --install-extension ms-vsliveshare.vsliveshare
+code --install-extension mtxr.sqltools
+code --install-extension mtxr.sqltools-driver-pg
+code --install-extension Pivotal.vscode-boot-dev-pack
+code --install-extension Pivotal.vscode-concourse
+code --install-extension Pivotal.vscode-manifest-yaml
+code --install-extension Pivotal.vscode-spring-boot
+code --install-extension redhat.java
+code --install-extension redhat.vscode-xml
+code --install-extension VisualStudioExptTeam.vscodeintellicode
+code --install-extension vscjava.vscode-java-debug
+code --install-extension vscjava.vscode-java-dependency
+code --install-extension vscjava.vscode-java-pack
+code --install-extension vscjava.vscode-java-test
+code --install-extension vscjava.vscode-maven
+code --install-extension vscjava.vscode-spring-boot-dashboard
+code --install-extension vscjava.vscode-spring-initializr
